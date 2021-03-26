b6f98f27085f7da45ed2f404aa9bf4c2e8230afa
Koh Woo Hyuk <whkoh7@naver.com>
2021-03-27 02:34:18 +0900

    카메라 전환 문제 해결

--- Client/D3D12Project01-블러링/Player.cpp
+++ Client/D3D12Project01-블러링/Player.cpp
@@ -378,7 +378,8 @@ CCamera *CAirplanePlayer::ChangeCamera(DWORD nNewCameraMode, float fTimeElapsed)
 			m_pCamera = OnChangeCamera(THIRD_PERSON_CAMERA, nCurrentCameraMode);
 			m_pCamera->SetTimeLag(0.25f);
 			m_pCamera->SetOffset(XMFLOAT3(0.0f, 15.0f, -30.0f));
-			m_pCamera->SetPosition(Vector3::Add(m_xmf3Position, m_pCamera->GetOffset()));
+			m_pCamera->SetPosition(Vector3::Add(m_xmf3Position, Vector3::ScalarProduct(Vector3::Normalize(m_xmf3Look), -1 * Vector3::Length(m_pCamera->GetOffset()))));
+			m_pCamera->SetLookAt(m_xmf3Position);
 			m_pCamera->GenerateProjectionMatrix(1.01f, 5000.0f, ASPECT_RATIO, 60.0f);
 			m_pCamera->SetViewport(0, 0, FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT, 0.0f, 1.0f);
 			m_pCamera->SetScissorRect(0, 0, FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT);
