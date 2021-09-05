42fec342c0a98f9c38693350389f02998b2227ec
Koh Woo Hyuk <whkoh7@naver.com>
2021-09-05 17:05:58 +0900

    혼자서도 결과창 나옴

--- Client/SkyFall/GameFramework.cpp
+++ Client/SkyFall/GameFramework.cpp
@@ -1332,7 +1332,6 @@ void CGameFramework::FrameAdvance()
     }
 
 	UpdateShadowMap();
-    m_pScene->UpdateMap();
 	if(m_pScene->GetState() == SCENE::INGAME)
 		UpdateMiniMap();
 
@@ -1611,7 +1610,6 @@ void CGameFramework::Restart()
     m_pScene->m_ppUIObjects[2]->SetAlpha(0.0f);
 
     m_pScene->Reset();
-    //m_pScene->m_pMap->test();
     m_pScene->m_iState = SCENE::LOBBY;
 
     m_pPlayer = m_pScene->m_pPlayer = pBasicPlayer;
